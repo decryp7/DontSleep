13ccefcdf1ff50f0a4acfe0214939acbccc4badd
Teo Kok Yong <KokYong.Teo@sg.yokogawa.com>
2020-10-29 15:04:02 +0800

    Don't sleep my PC!!!!

---
+++ DontSleep/NativeMethods.cs
@@ -0,0 +1,45 @@
+﻿#region FileHeader
+// (c) Copyright 2020 - Yokogawa Electric Corporation.
+// All Right Reserved
+// 
+// This software [both binary and source (if released)] (hereafter, Software)
+// is intellectual property owned by Yokogawa Electric Corporation and is
+// copyright of Yokogawa Electric Corporation in all countries in the world,
+// and ownership remains with Yokogawa Electric Corporation.
+// 
+// Origin:
+// 
+// Description:
+#endregion
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace DontSleep
+{
+    internal static class NativeMethods
+    {
+
+        public static void PreventSleep()
+        {
+            SetThreadExecutionState(ExecutionState.EsContinuous | ExecutionState.EsSystemRequired);
+        }
+
+        public static void AllowSleep()
+        {
+            SetThreadExecutionState(ExecutionState.EsContinuous);
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+
+        [Flags]
+        private enum ExecutionState : uint
+        {
+            EsAwaymodeRequired = 0x00000040,
+            EsContinuous = 0x80000000,
+            EsDisplayRequired = 0x00000002,
+            EsSystemRequired = 0x00000001
+        }
+    }
+}
---
+++ DontSleep/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DontSleep
+{
+    class Program
+    {
+        private static readonly ManualResetEvent waitEvent = new ManualResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            NativeMethods.PreventSleep();
+            //wait forever
+            waitEvent.WaitOne();
+        }
+    }
+}
